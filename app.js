df897854bdb762b0106ad922c216eebd61203b8a
s12171934 <ssong99999@naver.com>
2024-05-28 17:22:58 +0900

    REFACTOR: app.js

--- app.js
+++ app.js
@@ -3,21 +3,28 @@
   const boardRoutes = require('./src/routes/BoardRoutes');
   const config = await require('./src/config/springConfigClient');
   const eureka = require('./src/config/EurekaClientConfig');
-
-  const port = config.port;
-
-  const eurekaClient = eureka.createEurekaClient(config);
-  eurekaClient.start();
-
+  
   const app = express();
 
+  //body-parser 대체
   app.use(express.json());
   app.use(express.urlencoded({ extended: false }));
+
+  //routes 설정
+  app.use('/board', boardRoutes);
+
+  //port번호 설정
+  const port = config.port;
+
+  //포트 지정
   app.listen(port, () => {
     console.log(`listen to ${port}`)
   });
 
-  app.use('/board', boardRoutes);
+  //eureka client 설정
+  const eurekaClient = eureka.createEurekaClient(config);
+  eurekaClient.start();
+
   
   module.exports = app;
 })();

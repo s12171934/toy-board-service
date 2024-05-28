73e5ba1d0f6959ae2cbadccdb2dd673902500e99
s12171934 <ssong99999@naver.com>
2024-05-28 17:19:26 +0900

    FEAT: eureka client

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 (async function(){
   const express = require('express');
-  const config = await require('./src/springConfigClient');
-  const app = express();
   const boardRoutes = require('./src/routes/BoardRoutes');
+  const config = await require('./src/config/springConfigClient');
+  const eureka = require('./src/config/EurekaClientConfig');
 
   const port = config.port;
+
+  const eurekaClient = eureka.createEurekaClient(config);
+  eurekaClient.start();
+
+  const app = express();
 
   app.use(express.json());
   app.use(express.urlencoded({ extended: false }));
